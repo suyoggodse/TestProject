ecc9bea9bd9d911448f4935f2f0cb0bf1482d950
suyog Godse <suyog.godse@staples.com>
2019-05-09 03:35:46 -0700

    calc.java is added

---
+++ CALC.java
@@ -0,0 +1,18 @@
+class CALC
+{
+	public static int add (int a, int b)
+	{
+		int c;
+		c=a+b;
+		System.out.println ("Addition of Two numbers " + a + " " + b +"is:"+ c);
+		return c ;
+	}
+	public static int mult (int a, int b)
+	{
+		int c;
+		c=a*b;
+		System.out.println (c);
+		return  (c);
+	
+	}
+}
